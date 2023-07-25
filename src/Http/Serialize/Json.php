d76d2194fa02cd0d8c823e4454dfb7765260b76e
MICHAEL TEA <pika_79@hotmail.com>
2023-07-25 11:45:27 +0800

    fixed tests

--- src/Http/Serialize/Json.php
+++ src/Http/Serialize/Json.php
@@ -4,15 +4,33 @@ declare(strict_types=1);
 
 namespace Meilisearch\Http\Serialize;
 
+use Meilisearch\Exceptions\JsonDecodingException;
+use Meilisearch\Exceptions\JsonEncodingException;
+
 class Json implements SerializerInterface
 {
+    private const JSON_ENCODE_ERROR_MESSAGE = 'Encoding payload to json failed: "%s".';
+    private const JSON_DECODE_ERROR_MESSAGE = 'Decoding payload to json failed: "%s".';
+
     public function serialize($data)
     {
-        return json_encode($data);
+        try {
+            $encoded = json_encode($data, 4194304);
+        } catch (\JsonException $e) {
+            throw new JsonEncodingException(sprintf(self::JSON_ENCODE_ERROR_MESSAGE, $e->getMessage()), $e->getCode(), $e);
+        }
+
+        return $encoded;
     }
 
     public function unserialize(string $string)
     {
-        return json_decode($string, true);
+        try {
+            $decoded = json_decode($string, true, 512, 4194304);
+        } catch (\JsonException $e) {
+            throw new JsonDecodingException(sprintf(self::JSON_DECODE_ERROR_MESSAGE, $e->getMessage()), $e->getCode(), $e);
+        }
+
+        return $decoded;
     }
 }
